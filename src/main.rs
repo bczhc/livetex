7c21d61cf6b81f7cac53f4b71a1117aafd483dfb
Zhai Can <bczhc0@126.com>
2025-01-12 16:54:14 +0800

    use async + hyper

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,46 @@
 use clap::Parser;
 use livetex::Args;
 use log::{error, info};
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::net::{SocketAddr, TcpStream};
 use std::str::FromStr;
 use std::thread::spawn;
 use std::{env, io};
 use std::io::{BufRead, BufReader, Read};
+use std::convert::Infallible;
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
-fn main() -> anyhow::Result<()> {
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<() >{
     // enable info logging mode by default
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
     let args = Args::parse();
     info!("Listening on {}...", args.addr);
-    let listener = TcpListener::bind(SocketAddr::from_str(&args.addr)?)?;
+    let listener = TcpListener::bind(SocketAddr::from_str(&args.addr)?).await?;
+
     loop {
-        match listener.accept() {
-            Ok(s) => {
-                spawn(move || {
-                    info!("Accepted: {}", s.1);
-                    let result = handle_client(s.0);
-                    if let Err(e) = result {
-                        error!("Error occurred on handling client: {}", e);
-                        panic!("{}", e);
-                    }
-                });
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
             }
-            Err(e) => {
-                error!("Failure on accept: {}", e);
-            }
-        }
+        });
     }
 }
 
-fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
-    use io::Write;
-    let mut request = String::new();
-    BufReader::new(stream.try_clone()?).read_line(&mut request)?;
-    println!("{}", request);
-    
-    writeln!(&mut stream, "{}", "HTTP/1.1 200 OK\r\n\r\nhello, world")?;
-    drop(stream);
-
-    Ok(())
-}
